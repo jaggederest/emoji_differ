c2d43e229ba6b1c473b138f027b519e5f28e1a94
Justin George <justin.george@instacart.com>
2019-02-15 14:34:39 -0800

    basic Gem skeleton

---
+++ lib/emoji_differ.rb
@@ -0,0 +1,6 @@
+require "emoji_differ/version"
+
+module EmojiDiffer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/emoji_differ/version.rb
@@ -0,0 +1,3 @@
+module EmojiDiffer
+  VERSION = "0.1.0"
+end
